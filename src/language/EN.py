d0b9f2816e6852e0a6cd587690bc6fd1ec1a0772
azare <alirezaz1398@outlook.com>
2024-02-27 15:46:16 +0100

    [ERR] langue -> bien dit

---
+++ src/language/EN.py
@@ -0,0 +1,5 @@
+import src.language.Langue as Langue
+
+class LangueEn(Langue.Langue):
+    def bien_dit(self):
+        return "Well said"
---
+++ src/language/FR.py
@@ -0,0 +1,6 @@
+import src.language.Langue as Langue
+
+
+class LangueFr(Langue.Langue):
+    def bien_dit(self):
+        return "Bien dit"
