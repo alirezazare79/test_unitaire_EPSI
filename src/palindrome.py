93dc029c5de51766d5150a9c022f55c9ea6c587b
azare <alirezaz1398@outlook.com>
2024-02-27 15:28:51 +0100

    [TRUE] palindrome

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,6 +1,12 @@
+import os
+
+
 class Palindrome:
     def mirroir(self, mot):
         return mot[::-1]
 
     def palindrome(self, mot):
-        return mot
+        resultat = mot
+        if mot == self.mirroir(mot):
+            resultat += os.linesep + "bien dit"
+        return resultat
