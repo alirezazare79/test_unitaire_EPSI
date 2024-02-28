1c76afe3220a952931b90b885fee79f6248ad3f1
azare <alirezaz1398@outlook.com>
2024-02-28 22:54:17 +0100

    [TRUE] periode journee -> bonjour

--- src/language/EN.py
+++ src/language/EN.py
@@ -1,12 +1,23 @@
 import src.language.Langue as Langue
+from src.moment import Moment
 
 
 class LangueEn(Langue.Langue):
     def bien_dit(self):
         return "Well said"
 
-    def dit_bonjour(self):
-        return "Hello"
+    def dit_bonjour(self, moment):
+        match moment:
+            case Moment.MATIN:
+                return "Good morning"
+            case Moment.APRES_MIDI:
+                return "Good afternoon"
+            case Moment.SOIR:
+                return "Good evening"
+            case Moment.NUIT:
+                return "Good night"
+            case _:
+                return "Hello"
 
     def au_revoir(self):
         return "Goodbye"
--- src/language/FR.py
+++ src/language/FR.py
@@ -1,12 +1,23 @@
 import src.language.Langue as Langue
+from src.moment import Moment
 
 
 class LangueFr(Langue.Langue):
     def bien_dit(self):
         return "Bien dit"
 
-    def dit_bonjour(self):
-        return "Bonjour"
+    def dit_bonjour(self, moment):
+        match moment:
+            case Moment.MATIN:
+                return "Bonjour"
+            case Moment.APRES_MIDI:
+                return "Bon après-midi"
+            case Moment.SOIR:
+                return "Bonsoir"
+            case Moment.NUIT:
+                return "Bonne nuit"
+            case _:
+                return "Salut"
 
     def au_revoir(self):
         return "Au revoir"
